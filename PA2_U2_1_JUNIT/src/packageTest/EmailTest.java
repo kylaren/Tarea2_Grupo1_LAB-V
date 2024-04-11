3b782851a0363afe0a288e6bf27601fcb17ce766
Lisandro Jiménez <lisandro.25.lj@gmail.com>
2024-04-11 17:27:39 -0300

    Modificacion de test y nuevos agregados

--- PA2_U2_1_JUNIT/src/packageTest/EmailTest.java
+++ PA2_U2_1_JUNIT/src/packageTest/EmailTest.java
@@ -3,111 +3,109 @@ package packageTest;
 import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import frgp.utn.edu.ar.entidad.Email;
 
 class EmailTest {
 	
-	private String correo; 
-	private Email email; 
 	
-	
-	@BeforeEach
-	public void beforeEachTest() {
-
-	}
-	
-	@AfterEach
-	public void afterEachTest() {
-		
-	}
 	// Métodos de prueba para validar correo con letra mayúscula
 	@Test
 	public void testValidarCorreo_conLetraMayuscula_retornaTrue() {
-		correo = "EjemPlo1@gmail.com"; 
+		/*correo = "EjemPlo1@gmail.com"; 
 		email = new Email(correo); 
-		boolean res = Email.validarCorreo(email.toString());
+		boolean res = Email.validarCorreo(email.toString());		
+		assertTrue(res);*/
 		
-		assertTrue(res); // Verifica que el resultado sea verdadero
+		assertTrue(Email.validarCorreo("EjemPlo1@gmail.com"));
 	}
 	
 	@Test
-	public void testValidarCorreo_sinLetraMayuscula_retornaFalse() {
-
-	}
-	
-	
-	// Métodos de prueba para validar correo con letra minúscula
-	@Test
-	public void testValidarCorreo_conLetraMinuscula_retornaTrue() {
-		
+	public void testValidarCorreo_MasDeOchoDigitos_ConNumero_TodasMinusculas() {
+		assertFalse(Email.validarCorreo("ejemplo2@gmail.com"));
 	}
 	
 	@Test
-	public void testValidarCorreo_sinLetraMinuscula_retornaFalse() {
-		
+	public void testValidarCorreo_MasDeOchoDigitos_ConNumero_TodasMayusculas() {
+		assertFalse(Email.validarCorreo("EJEMPLO2@GMAIL.COM"));
 	}
+	
+	@Test
+	public void testValidarCorreo_MasDeOchoDigitos_SoloNumeros() {
+		assertFalse(Email.validarCorreo("112233@6.0"));
+	}
+	
+	@Test
+	public void testValidarCorreo_MenosDeOchoDigitos_SoloMinusculas() {
+		assertFalse(Email.validarCorreo("ej@mail"));
+	}
+	
+	@Test
+	public void testValidarCorreo_MenosDeOchoDigitos_SoloMatusculas() {
+		assertFalse(Email.validarCorreo("EJ@MAIL"));
+	}
+	
+	
 	
 	@Test
 	public void testValidarCorreo_alMenosUnNumero_retornaTrue() {
-		correo = "EjemPlo1@gmail.com"; 
+		/*correo = "EjemPlo1@gmail.com"; 
 		email = new Email(correo); 
 		boolean res = Email.validarCorreo(email.toString());
-		assertTrue(res); 
+		assertTrue(res); */
+		assertTrue(Email.validarCorreo("EjemPlo1@gmail.com"));
 	}
+	
 	@Test
 	public void testValidarCorreo_alMenosUnNumero_retornaFalse() {
-		correo = "EjemPlo@gmail.com"; 
+		/*correo = "EjemPlo@gmail.com"; 
 		email = new Email(correo); 
-		boolean res = Email.validarCorreo(email.toString());
-		assertFalse(res); 
+		boolean res = Email.validarCorreo(email.toString());*/
+		assertFalse(Email.validarCorreo("EjemPlo@gmail.com")); 
+		
 	}
 	
 	//Ocho digitos
 	
     @Test
-    public void testValidarCorreo_Al_Menos_8_Digitos_retortnaTrue() {
+    public void testValidarCorreo_Al_Menos_8_Digitos_retornaTrue() {
     	
-    	correo = "12345678@correo.com"; 
+    	/*correo = "12345678@correo.com"; 
 		email = new Email(correo); 
-		boolean res = Email.validarCorreo(email.toString());
+		boolean res = Email.validarCorreo(email.toString());*/
 		
-		assertTrue(res); 
+		assertTrue(Email.validarCorreo("12345678@correo.com")); 
+    }
+    
+    
+    @Test
+    public void testValidarCorreo_Mas_De_8_Digitos_retornaTrue() {
+    	
+    	/*correo = "1234567891011@correo.com"; 
+		email = new Email(correo); 
+		boolean res = Email.validarCorreo(email.toString());*/
+		
+		assertTrue(Email.validarCorreo("1234567891011@correo.com")); 
     }
     
     @Test
-    public void testValidarCorreo_Al_Menos_8_Digitos_retortnaFalse() {
+    public void testValidarCorreo_Al_Menos_8_Digitos_retornaFalse() {
     	
-    	correo = "1234567@correo.com"; 
+    	/*correo = "1234567@correo.com"; 
 		email = new Email(correo); 
-		boolean res = Email.validarCorreo(email.toString());
-		
-		assertFalse(res); 
+		boolean res = Email.validarCorreo(email.toString());*/
+    	
+    	assertFalse(Email.validarCorreo("1234567@correo.com")); 
     }
-    
     @Test
-    public void testValidarCorreo_Mas_De_8_Digitos_retortnaTrue() {
+    public void testValidarCorreo_Sin_Digitos_retornaFalse() {
     	
-    	correo = "1234567891011@correo.com"; 
+    	/*correo = ""; 
 		email = new Email(correo); 
-		boolean res = Email.validarCorreo(email.toString());
+		boolean res = Email.validarCorreo(email.toString());*/
 		
-		assertTrue(res); 
-    }
-    
-    @Test
-    public void testValidarCorreo_Sin_Digitos_retortnaFalse() {
-    	
-    	correo = ""; 
-		email = new Email(correo); 
-		boolean res = Email.validarCorreo(email.toString());
-		
-		assertFalse(res); 
+		assertFalse(Email.validarCorreo("")); 
     }
     
 }
